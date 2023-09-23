7c82ace9495b448a91a9cc1b6e6bae0634154b9a
Paweł Gargula <pawel.gargula@interia.pl>
2023-09-23 09:51:50 +0200

    Starting point

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,115 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      
+        {/* <!-- Sidebar start --> */}
+
+      Step 1
+      Your info
+
+      Step 2
+      Select plan
+
+      Step 3
+      Add-ons
+
+      Step 4
+      Summary
+
+      {/* <!-- Sidebar end --> */}
+
+      {/* <!-- Step 1 start --> */}
+
+      Personal info
+      Please provide your name, email address, and phone number.
+
+      Name
+      e.g. Stephen King
+
+      Email Address
+      e.g. stephenking@lorem.com
+
+      Phone Number
+      e.g. +1 234 567 890
+
+      Next Step
+
+      {/* <!-- Step 1 end --> */}
+
+      {/* <!-- Step 2 start --> */}
+
+      Select your plan
+      You have the option of monthly or yearly billing.
+
+      Arcade
+      $9/mo
+
+      Advanced
+      $12/mo
+
+      Pro
+      $15/mo
+
+      Monthly
+      Yearly
+
+      Go Back
+      Next Step
+
+      {/* <!-- Step 2 end --> */}
+
+      {/* <!-- Step 3 start --> */}
+
+      Pick add-ons
+      Add-ons help enhance your gaming experience.
+
+      Online service
+      Access to multiplayer games
+      +$1/mo
+
+      Larger storage
+      Extra 1TB of cloud save
+      +$2/mo
+
+      Customizable Profile
+      Custom theme on your profile
+      +$2/mo
+
+      Go Back
+      Next Step
+
+      {/* <!-- Step 3 end --> */}
+
+      {/* <!-- Step 4 start --> */}
+
+      Finishing up
+      Double-check everything looks OK before confirming.
+
+      {/* <!-- Dynamically add subscription and add-on selections here --> */}
+
+      Total (per month/year)
+
+      Go Back
+      Confirm
+
+      {/* <!-- Step 4 end --> */}
+
+      {/* <!-- Step 5 start --> */}
+
+      Thank you!
+
+      Thanks for confirming your subscription! We hope you have fun 
+      using our platform. If you ever need support, please feel free 
+      to email us at support@loremgaming.com.
+
+      {/* <!-- Step 5 end --> */}
+
+      <div class="attribution">
+        Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+        Coded by <a href="#">Your Name Here</a>.
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
